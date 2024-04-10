1df06b3538f7502a652c4d5e96a591ceddba9b63
Shanmugamsham <abdeshanmugam@gmail.com>
2024-04-10 17:50:11 +0700

    new

---
+++ routs.js
@@ -0,0 +1,62 @@
+const routs=require("express").Router()
+const fs=require("fs")
+const timedateinfo = new Date();
+ const time = timedateinfo.toLocaleTimeString("en-US", {
+   hour12: false,
+ });
+const trimmedTime = time.split(":").join("_");
+const date = timedateinfo.toLocaleDateString();
+const trimmedDate = date.split("/").join("");
+const fileName = trimmedDate + "-" + trimmedTime;
+
+
+
+routs.post("/create",(req,res)=>{
+    try {
+       
+       fs.writeFile(`./file/${fileName}.txt`, time, (err) => {
+          if (err) {
+             res.status(400).send(`try again`)
+          }
+          else {
+            res.status(200).send(`Filename has been created`)
+    
+          }
+        });
+    } catch (error) {
+       res.status(400).send(`server error`)
+    }
+  
+ 
+   
+  })
+
+  routs.get("/retrieve",(req,res)=>{
+
+
+    try {
+       fs.readdir("./file", (err, files) => {
+          if (err) {
+            console.log(err);
+          } 
+          else {
+
+            res.status(200).send(`Retrieve Files:   ${files}    `)
+
+            
+          }
+        });
+       
+    } catch (error) {
+       res.status(400).send(`server error`)
+    }
+    
+    })
+    
+    
+
+
+
+
+
+module.exports=routs
